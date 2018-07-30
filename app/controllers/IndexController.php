247fd77d8b728a4f7db446b45228edc5c5b4e132
Wesley Teixeira <contato@wesleyteixeira.com.br>
2018-07-29 22:38:16 -0300

    correcao rota index principal

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,5 +1,10 @@
 <?php
 class IndexController extends \HXPHP\System\Controller
 {
-    
+    function __construct($foo = null)
+    {
+    	parent::__construct($configs);
+    	
+    	$this->redirectTo($this->configs->baseURI.'home');
+    }
 }
