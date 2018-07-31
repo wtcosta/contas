e8ee6d82f92aa06fa0f35c2a1dcd12a2888d4201
Wesley Teixeira <contato@wesleyteixeira.com.br>
2018-07-31 00:56:54 -0300

    inicio CRUD category

--- app/controllers/CategoryController.php
+++ app/controllers/CategoryController.php
@@ -25,7 +25,57 @@ class CategoryController extends \HXPHP\System\Controller
 			$role->role
 		);
 
-		$this->view->setTitle('WtContas - Categorias')
-					->setVar('user', $user);
+		$this->view
+		->setTitle('WtContas - Categorias')
+		->setVar('categorias', Category::all());
+	}
+
+	public function cadastrarAction()
+	{
+		//Redireciona para uma view
+		$this->view->setFile('index');
+
+		$post = $this->request->post();
+
+		//Verifica se o POST não está vazio e chama o model
+		if (!empty($post)) {
+			$cadCategoria = Category::cadastrar($post);
+			if ($cadCategoria->status === false) {
+				$this->load('Helpers\Alert', array(
+					'danger',
+					'Ops! Não foi possível efetuar seu cadastro.<br />Verifique os erros abaixo:',
+					$cadCategoria->errors
+				));
+			}else{
+				$this->load('Helpers\Alert', array(
+					'success',
+					'Cadastro realizado com sucesso!'
+				));
+				$this->view
+				->setVar('categorias', Category::all());
+			}
+		}
+	}
+
+	public function editarAction($cat)
+	{
+		$post = $this->request->post();
+
+		if (!empty($post)) {
+			$atualizarCad = Category::atualizar($cat, $post);
+
+			if ($atualizarCad->status === false) {
+				$this->load('Helpers\Alert', array(
+					'danger',
+					'Ops! Não foi possível editar o cadastro. <br> Verifique os erros abaixo:',
+					$atualizarCad->errors
+				));
+			}else{
+				$this->redirectTo($this->configs->baseURI.'usuarios/editado');
+			}
+		}
+
+		$this->view
+		->setVar('categorias', Category::all());
 	}
 }
--- app/models/Category.php
+++ app/models/Category.php
@@ -2,11 +2,11 @@
 
 class Category extends \HXPHP\System\Model
 {
-public static function cadastrar($post)
+	public static function cadastrar($post)
 	{
 		//Cria uma classe vazia pra armazenar o retorno das validações
 		$callbackObj = new \stdClass;
-		$callbackObj->user = null;
+		$callbackObj->cad = null;
 		$callbackObj->status = false;
 		$callbackObj->errors = array();
 
@@ -14,7 +14,34 @@ public static function cadastrar($post)
 		$cadastrar = self::create($post);
 
 		if ($cadastrar->is_valid()) {
-			$callbackObj->user = $cadastrar;
+			$callbackObj->cad = $cadastrar;
+			$callbackObj->status = true;
+			return $callbackObj;
+		}
+
+		$errors = $cadastrar->errors->get_raw_errors();
+
+		foreach ($errors as $field => $message) {
+			array_push($callbackObj->errors, $message[0]);
+		}
+
+		return $callbackObj;
+	}
+
+	public static function atualizar($cad_id, array $post)
+	{
+		$callbackObj = new \stdClass;
+		$callbackObj->cad = null;
+		$callbackObj->status = false;
+		$callbackObj->errors = array();
+
+		$cad = self::find($cad_id);
+		$cad->nome = $post['nome'];
+
+		$atualizar = $cad->save(false);
+
+		if ($atualizar) {
+			$callbackObj->cad = $cad;
 			$callbackObj->status = true;
 			return $callbackObj;
 		}
